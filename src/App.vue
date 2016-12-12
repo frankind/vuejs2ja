83b2258173ccd51cdbed4efe428bd5d55ff1ba99
Preecha Tangsongsermsuk <frankind@gmail.com>
2016-12-12 19:44:03 +0700

    [v-else-if] like v-if.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <input v-model="name">
-    <p v-if="name">Hi, {{ name }}</p>
+    <p v-if="name && name !== 'frankind' ">Hi, {{ name }}</p>
+    <p v-else-if="name === 'frankind'">:D</p>
     <p v-else>No Data</p>
   </div>
 </template>
