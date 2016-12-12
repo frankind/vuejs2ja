82f99835039852fc758d6092a5f17f88866d13b5
Preecha Tangsongsermsuk <frankind@gmail.com>
2016-12-12 19:31:19 +0700

    [d2][directive] v-text is using escape html.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+      <input v-model="name">
+      <p v-text="name"></p>
   </div>
 </template>
 
 <script>
   export default {
-
+    data () {
+      return {
+        name: ''
+      }
+    }
   }
 </script>
-
